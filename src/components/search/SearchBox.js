6f3ed37112e896aae587fc50662931ce25f156e6
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2023-08-15 11:40:30 +0330

    changed favicon

--- src/components/search/SearchBox.js
+++ src/components/search/SearchBox.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./SearchBox.css";
 import { BsSearch } from "react-icons/bs";
 
